9e1fdf1916aaafdf30ec90ed5e31df0eab4a2829
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-03-11 20:36:22 +0530

    Add todo list functionality to the home screen.

--- app/(tabs)/home/index.js
+++ app/(tabs)/home/index.js
@@ -1,11 +1,111 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { Feather } from "@expo/vector-icons";
 
 const index = () => {
+  const [todos, setTodos] = useState([]);
   return (
-    <View>
-      <Text>home</Text>
-    </View>
+    <>
+      <View
+        style={{
+          marginHorizontal: 10,
+          marginVertical: 10,
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 12,
+        }}
+      >
+        <Pressable
+          style={{
+            backgroundColor: "#7CB9E8",
+            paddingHorizontal: 10,
+            paddingVertical: 6,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "white", textAlign: "center" }}>All</Text>
+        </Pressable>
+        <Pressable
+          style={{
+            backgroundColor: "#7CB9E8",
+            paddingHorizontal: 10,
+            paddingVertical: 6,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "white", textAlign: "center" }}>Work</Text>
+        </Pressable>
+        <Pressable
+          style={{
+            backgroundColor: "#7CB9E8",
+            paddingHorizontal: 10,
+            paddingVertical: 6,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+            marginRight: "auto",
+          }}
+        >
+          <Text style={{ color: "white", textAlign: "center" }}>Personal</Text>
+        </Pressable>
+        <Pressable onPress={() => setModalVisible(!isModalVisible)}>
+          <Feather name="plus-circle" size={30} color="#007FFF" />
+        </Pressable>
+      </View>
+
+      <ScrollView style={{ flex: 1, backgroundColor: "white" }}>
+        <View style={{ padding: 10 }}>
+          {todos?.length > 0 ? (
+            <View></View>
+          ) : (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: 130,
+                marginLeft: "auto",
+                marginRight: "auto",
+              }}
+            >
+              <Image
+                style={{ width: 200, height: 200, resizeMode: "contain" }}
+                source={{
+                  uri: "https://cdn-icons-png.flaticon.com/128/2387/2387679.png",
+                }}
+              />
+              <Text
+                style={{
+                  fontSize: 16,
+                  marginTop: 15,
+                  fontWeight: "600",
+                  textAlign: "center",
+                }}
+              >
+                No Tasks for today! add a task
+              </Text>
+              <Pressable
+                onPress={() => setModalVisible(!isModalVisible)}
+                style={{ marginTop: 15 }}
+              >
+                <Feather name="plus-circle" size={30} color="#007FFF" />
+              </Pressable>
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
