7ec153f37108cfba0b6f09c0cbfd9857a576d913
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-03-12 00:01:39 +0530

    bottom modal ui done

--- app/(tabs)/home/index.js
+++ app/(tabs)/home/index.js
@@ -4,13 +4,55 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } from "react-native";
 import React, { useState } from "react";
 import { Feather } from "@expo/vector-icons";
+import { BottomModal, ModalContent, ModalTitle } from "react-native-modals";
+import { SlideAnimation } from "react-native-modals";
+import { Entypo, FontAwesome } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import axios from "axios";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const index = () => {
   const [todos, setTodos] = useState([]);
+  // const today = moment().format("MMM Do");
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [category, setCategory] = useState("All");
+  const [todo, setTodo] = useState("");
+  const suggestions = [
+    {
+      id: "0",
+      todo: "Drink Water, keep healthy",
+    },
+    {
+      id: "1",
+      todo: "Go Excercising",
+    },
+    {
+      id: "2",
+      todo: "Go to bed early",
+    },
+    {
+      id: "3",
+      todo: "Take pill reminder",
+    },
+    {
+      id: "4",
+      todo: "Go Shopping",
+    },
+    {
+      id: "5",
+      todo: "finish assignments",
+    },
+  ];
+  const addTodo = () => {
+    setModalVisible(false);
+    setTodo("");
+  };
+
   return (
     <>
       <View
@@ -105,6 +147,120 @@ const index = () => {
           )}
         </View>
       </ScrollView>
+
+      <BottomModal
+        onBackdropPress={() => setModalVisible(!isModalVisible)}
+        onHardwareBackPress={() => setModalVisible(!isModalVisible)}
+        swipeDirection={["up", "down"]}
+        swipeThreshold={200}
+        modalTitle={<ModalTitle title="Add a todo" />}
+        modalAnimation={
+          new SlideAnimation({
+            slideFrom: "bottom",
+          })
+        }
+        visible={isModalVisible}
+        onTouchOutside={() => setModalVisible(!isModalVisible)}
+      >
+        <ModalContent style={{ width: "100%", height: 280 }}>
+          <View
+            style={{
+              marginVertical: 10,
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+            }}
+          >
+            <TextInput
+              value={todo}
+              onChangeText={(text) => setTodo(text)}
+              placeholder="Input a new task here"
+              style={{
+                padding: 10,
+                borderColor: "#E0E0E0",
+                borderWidth: 1,
+                borderRadius: 5,
+                flex: 1,
+              }}
+            />
+            <Ionicons onPress={addTodo} name="send" size={24} color="#007FFF" />
+          </View>
+
+          <Text>Choose Category</Text>
+
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              marginVertical: 10,
+            }}
+          >
+            <Pressable
+              onPress={() => setCategory("Work")}
+              style={{
+                borderColor: "#E0E0E0",
+                paddingHorizontal: 10,
+                paddingVertical: 4,
+                borderWidth: 1,
+                borderRadius: 25,
+              }}
+            >
+              <Text>Work</Text>
+            </Pressable>
+            <Pressable
+              onPress={() => setCategory("Personal")}
+              style={{
+                borderColor: "#E0E0E0",
+                paddingHorizontal: 10,
+                paddingVertical: 4,
+                borderWidth: 1,
+                borderRadius: 25,
+              }}
+            >
+              <Text>Personal</Text>
+            </Pressable>
+            <Pressable
+              onPress={() => setCategory("WishList")}
+              style={{
+                borderColor: "#E0E0E0",
+                paddingHorizontal: 10,
+                paddingVertical: 4,
+                borderWidth: 1,
+                borderRadius: 25,
+              }}
+            >
+              <Text>WishList</Text>
+            </Pressable>
+          </View>
+
+          <Text>Some sugggestions</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              flexWrap: "wrap",
+              marginVertical: 10,
+            }}
+          >
+            {suggestions?.map((item, index) => (
+              <Pressable
+                // onPress={() => setTodo(item?.todo)}
+                style={{
+                  backgroundColor: "#F0F8FF",
+                  paddingHorizontal: 10,
+                  paddingVertical: 4,
+                  borderRadius: 25,
+                }}
+                key={index}
+              >
+                <Text style={{ textAlign: "center" }}>{item?.todo}</Text>
+              </Pressable>
+            ))}
+          </View>
+        </ModalContent>
+      </BottomModal>
     </>
   );
 };
