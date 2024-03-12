f3cb2e303f88063f23784ed89317b07d3823e095
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-03-12 14:22:09 +0530

    calendar tab done

--- api/index.js
+++ api/index.js
@@ -142,3 +142,37 @@ app.patch("/todos/:todoId/complete", async (req, res) => {
     res.status(500).json({ error: "Something went wrong" });
   }
 });
+
+app.get("/todos/completed/:date", async (req, res) => {
+  try {
+    const date = req.params.date;
+
+    const completedTodos = await Todo.find({
+      status: "completed",
+      createdAt: {
+        $gte: new Date(`${date}T00:00:00.000Z`), // Start of the selected date
+        $lt: new Date(`${date}T23:59:59.999Z`), // End of the selected date
+      },
+    }).exec();
+
+    res.status(200).json({ completedTodos });
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.get("/todos/count", async (req, res) => {
+  try {
+    const totalCompletedTodos = await Todo.countDocuments({
+      status: "completed",
+    }).exec();
+
+    const totalPendingTodos = await Todo.countDocuments({
+      status: "pending",
+    }).exec();
+
+    res.status(200).json({ totalCompletedTodos, totalPendingTodos });
+  } catch (error) {
+    res.status(500).json({ error: "Network error" });
+  }
+});
